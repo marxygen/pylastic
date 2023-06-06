466c7b5f90cc3931d29795ee013ff404dd00eff0
Mark Ivchenko <marxygen@gmail.com>
2023-06-07 00:16:25 +0500

    Fix batch generation test

--- tests/utils/test_iterables.py
+++ tests/utils/test_iterables.py
@@ -18,10 +18,9 @@ def test_string():
 
 
 def test_get_batches_with_size():
-    objects = [random.randint(1, 1**10) for _ in range(random.randint(100, 1_000))]
+    objects = [random.randint(1, 10**10) for _ in range(random.randint(100, 1_000))]
     total_size = full_size_of(objects)
 
     expected_batches = random.randint(1, 10)
     batch_size = ceil(total_size / expected_batches)
-
-    assert len(get_batches_with_size(objects, batch_size)) == expected_batches
+    assert len(get_batches_with_size(objects, batch_size)) == expected_batches + 1
